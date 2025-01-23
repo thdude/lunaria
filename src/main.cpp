03b916aeef63cf4e446f413a9f92f51627bacd54
thdude <30702113+thdude@users.noreply.github.com>
2025-01-23 05:40:48 -0700

    a real program

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+    cout << "Hello World"; // prints Hello World
+    while(true)
+    {
+        cout << "Hello World";
+    }
+    //return 0;
+}
