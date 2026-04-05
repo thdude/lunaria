4f99bb0f5ba1a53a5d4723f82be6c29822e09073
thdude <30702113+thdude@users.noreply.github.com>
2026-04-04 21:02:31 -0600

    Update modelasset.cpp

--- src/resource/modelasset.cpp
+++ src/resource/modelasset.cpp
@@ -2,11 +2,40 @@
 #include "fastgltf/core.hpp"
 #include <string>
 #include <filesystem>
+#include <iostream>
 
 namespace lunaria
 {
-    void ModelAsset::LoadFromFile(std::filesystem::path path)
+    bool ModelAsset::LoadFromFile(std::filesystem::path path)
     {
+        {
+            static constexpr auto supportedExtensions =
+                fastgltf::Extensions::KHR_mesh_quantization |
+                fastgltf::Extensions::KHR_texture_transform |
+                fastgltf::Extensions::KHR_materials_variants;
 
+            fastgltf::Parser parser(supportedExtensions);
+
+            constexpr auto gltfOptions =
+                fastgltf::Options::DontRequireValidAssetMember |
+                fastgltf::Options::AllowDouble |
+                fastgltf::Options::LoadExternalBuffers |
+                fastgltf::Options::LoadExternalImages |
+                fastgltf::Options::GenerateMeshIndices;
+
+            auto gltfFile = fastgltf::MappedGltfFile::FromPath(path);
+            if (!bool(gltfFile)) {
+                std::cerr << "Failed to open glTF file: " << fastgltf::getErrorMessage(gltfFile.error()) << '\n';
+                return false;
+            }
+
+            auto asset = parser.loadGltf(gltfFile.get(), path.parent_path(), gltfOptions);
+            if (asset.error() != fastgltf::Error::None) {
+                std::cerr << "Failed to load glTF: " << fastgltf::getErrorMessage(asset.error()) << '\n';
+                return false;
+            }
+
+            viewer->asset = std::move(asset.get());
+        }
     }
 }
